8a623468f785fe3577088dad1b37bfbefeec5e49
Cho-ui <christian.lindell@myy.haaga-helia.fi>
2022-05-08 21:45:08 +0300

    Basic moving works, async issues & flatlist not useable

--- components/Player.js
+++ components/Player.js
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native'
-import { StorageAccessFramework } from 'expo-file-system';
-import * as FileSystem from 'expo-file-system';
 import * as MediaLibrary from 'expo-media-library';
 import { Audio } from 'expo-av';
 
 export default function Recorder() {
-    const [internalFolderUri, setInternalFolderUri] = useState("");
-    const [cacheRecordings, setCacheRecordings] = useState([]);
     const [sound, setSound] = useState();
     const [audioAssets, setAudioAssets] = useState([]);
-    const [assetIndex, setAssetIndex] = useState();
 
     useEffect(() => {
         return sound
@@ -24,123 +19,94 @@ export default function Recorder() {
 
     async function getFiles() {
         try {
-            const album = StorageAccessFramework.getUriForDirectoryInRoot("mirecorder");
-
-            // Requests permissions for external directory
-            const permissions = await StorageAccessFramework.requestDirectoryPermissionsAsync(album);
-
-            if (permissions.granted) {
-                // Gets SAF URI from response
-                const albumUri = permissions.directoryUri;
-
-                // check that the correct folder was selected - test this later
-                if (!albumUri.includes("mirecorder")) {
-                    return;
-                }
-
-                const mediaLibraryPermissions = await MediaLibrary.requestPermissionsAsync();
-                if (!mediaLibraryPermissions.granted) {
-                    return;
-                }
-
-                // delete previous cache folder along with contents so cache is up-to-date with media library assets
-                const cDir = await FileSystem.getInfoAsync(FileSystem.cacheDirectory + "mirecorder");
-                if (cDir.exists) {
-                    await FileSystem.deleteAsync(cDir.uri);
-                }
-
-                // Copies files from external storage to internal cache storage 
-                await StorageAccessFramework.copyAsync({
-                    from: albumUri,
-                    to: FileSystem.cacheDirectory,
-                });
-
-                const outputDir = FileSystem.cacheDirectory + "mirecorder";
-                setInternalFolderUri(outputDir); // most likely not needed, but saved for now
-
-                // filenames in the internal cache storage
-                const outputContents = await FileSystem.readDirectoryAsync(outputDir);
-                setCacheRecordings(outputContents);
-
-                // get media asset objects from external "mirecorder"-album
-                const assetAlbum = await MediaLibrary.getAlbumAsync('mirecorder');
-                const assetTable = await MediaLibrary.getAssetsAsync({
-                    mediaType: "audio",
-                    album: assetAlbum,
-                    first: 500
-                }); // 500 files per PagedInfo page, if more files, needs a higher limit
-
-                setAudioAssets(assetTable.assets);
+            const mediaLibraryPermissions = await MediaLibrary.requestPermissionsAsync();
+            if (!mediaLibraryPermissions.granted) {
+                return;
             }
 
+            // get media asset objects from external "mirecorder"-album
+            const assetAlbum = await MediaLibrary.getAlbumAsync('mirecorder');
+            const assetTable = await MediaLibrary.getAssetsAsync({
+                mediaType: "audio",
+                album: assetAlbum,
+                first: 500
+            }); // 500 files per PagedInfo page, if more files, needs a higher limit
+
+            setAudioAssets(assetTable.assets);
         } catch (err) {
             console.error(err);
         }
     }
 
-    async function playClip(clipFileName) {
+    async function playClip(asset) {
         try {
-            let assetToPlay = undefined;
-            for (const asset of audioAssets) {
-                if (asset.filename === clipFileName) assetToPlay = asset;
-            }
-
-            if (assetToPlay != undefined) {
-                const { sound } = await Audio.Sound.createAsync(assetToPlay);
-                setSound(sound);
-                console.log('Playing Sound');
-                await sound.playAsync();
-            }
-
+            const { sound } = await Audio.Sound.createAsync(asset);
+            setSound(sound);
+            console.log('Playing Sound');
+            await sound.playAsync();
         } catch (err) {
-            console.error(err);
+            console.error(err)
         }
     }
 
     async function playNext(asset, assetIndex) {
         try {
             const { sound } = await Audio.Sound.createAsync(asset);
-                setSound(sound);
-                sound.setOnPlaybackStatusUpdate((playbackStatus) => {
-                    const nextIndex = assetIndex + 1;
-                    if (playbackStatus.didJustFinish && nextIndex < audioAssets.length) {
-                        playNext(audioAssets[nextIndex], nextIndex);
-                    }
-                })
-                console.log('Playing Sound');
-                await sound.playAsync();
+            setSound(sound);
+            sound.setOnPlaybackStatusUpdate((playbackStatus) => {
+                const nextIndex = assetIndex + 1;
+                if (playbackStatus.didJustFinish && nextIndex < audioAssets.length) {
+                    playNext(audioAssets[nextIndex], nextIndex);
+                }
+            })
+            console.log('Playing Sound');
+            await sound.playAsync();
         } catch (err) {
             console.error(err);
         }
     }
 
-    // TODO: rename cacheRecordings to something more descriptive
     async function playAll() {
         try {
             const asset = audioAssets[0];
             const assetIndex = 0;
             const { sound } = await Audio.Sound.createAsync(asset);
-                setSound(sound);
-                sound.setOnPlaybackStatusUpdate((playbackStatus) => {
-                    if (playbackStatus.didJustFinish) {
-                        const nextIndex = assetIndex + 1;
-                        playNext(audioAssets[nextIndex], nextIndex);
-                    };
-                });
-                console.log('Playing Sound');
-                await sound.playAsync();
+            setSound(sound);
+            sound.setOnPlaybackStatusUpdate((playbackStatus) => {
+                if (playbackStatus.didJustFinish) {
+                    const nextIndex = assetIndex + 1;
+                    playNext(audioAssets[nextIndex], nextIndex);
+                };
+            });
+            console.log('Playing Sound');
+            await sound.playAsync();
         } catch (err) {
             console.error(err);
         }
     }
 
-    // maybe change to a flatlist
+    // Works, but async issues
+    function moveUp(index) {
+
+        if (index != 0) {
+            const asset = audioAssets[index];
+            let assetArrayCopy = audioAssets;
+            assetArrayCopy.splice(index, 1);
+            assetArrayCopy.splice((index - 1), 0, asset);
+            setAudioAssets(assetArrayCopy);
+        }
+    }
+
+    // Problems with a flatlist, not ideal this way either
     function renderPlaylist() {
-        return cacheRecordings.map((clipFileName, index) => {
+        return audioAssets.map((asset, index) => {
             return (
                 <View key={index} style={styles.row}>
                     <Text style={styles.fill}>Clip {index + 1}</Text>
-                    <Button style={styles.button} onPress={() => playClip(clipFileName)} title="Play" />
+                    <Button style={styles.button} onPress={() => moveUp(index)} title="Up" />
+                    <Button style={styles.button} title="Down" />
+                    <Button style={styles.button} onPress={() => playClip(asset)} title="Play" />
+                    <Button style={styles.button} title="Share" />
                 </View>
             );
         });
